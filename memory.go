a90475d552846ee29a6a0dc548b2d842ad142eec
Katharine <kekerr@gmail.com>
2021-03-29 00:33:16 -0700

    in-memory version of storage

---
+++ memory.go
@@ -0,0 +1,35 @@
+package distkvs
+
+import (
+	"sync"
+)
+
+type Memory struct {
+	kvs             map[string]string
+	mu               sync.Mutex
+}
+
+func NewMemory() *Memory {
+	return &Memory{
+		kvs:		make(map[string]string),
+	}
+}
+
+func (m *Memory) Get(key string) *string {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	value, ok := m.kvs[key]
+	if ok {
+		return &value
+	} else {
+		return nil
+	}
+}
+
+func (m *Memory) Put(key string, value string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.kvs[key] = value
+}
--- storage.go
+++ storage.go
@@ -1,9 +1,9 @@
 package distkvs
 
 import (
-	"errors"
-
+	"fmt"
 	"github.com/DistributedClocks/tracing"
+	"net/rpc"
 )
 
 type StorageConfig struct {
@@ -39,10 +39,91 @@ type StorageGetResult struct {
 	Value *string
 }
 
-type Storage struct {
-	// state may go here
+type StorageGetArgs struct {
+	Key				string
+	Token 			tracing.TracingToken
 }
 
-func (*Storage) Start(frontEndAddr string, storageAddr string, diskPath string, strace *tracing.Tracer) error {
-	return errors.New("not implemented")
+type StorageGetReply struct {
+	Value			*string
+	Token 			tracing.TracingToken
 }
+
+type StoragePutArgs struct {
+	Key				string
+	Value           string
+	Token 			tracing.TracingToken
+}
+
+type StoragePutReply struct {
+	Token 			tracing.TracingToken
+}
+
+type Storage struct {
+	frontEndAddr    string
+	storageAddr     string
+	frontEnd 		*rpc.Client
+	diskPath        string
+	tracer			*tracing.Tracer
+	memory			*Memory
+}
+
+func (s *Storage) Start(frontEndAddr string, storageAddr string, diskPath string, strace *tracing.Tracer) error {
+	// create RPC connection to frontend
+	frontEnd, err := rpc.Dial("tcp", frontEndAddr)
+	if err != nil {
+		return fmt.Errorf("error connecting with frontend: %s", err)
+	}
+
+	// initialize properties
+	s.frontEndAddr = frontEndAddr
+	s.storageAddr = storageAddr
+	s.frontEnd = frontEnd
+	s.diskPath = diskPath
+	s.tracer = strace
+	s.memory = NewMemory()
+
+	return nil
+}
+
+// Get is a blocking async RPC from the Frontend
+// fetching the value from memory
+func (s *Storage) Get(args StorageGetArgs, reply *StorageGetReply) {
+	trace := s.tracer.ReceiveToken(args.Token)
+
+	trace.RecordAction(StorageGet{
+		Key: args.Key,
+	})
+
+	value := s.memory.Get(args.Key)
+
+	trace.RecordAction(StorageGetResult{
+		Key: args.Key,
+		Value: value,
+	})
+
+	reply.Value = value
+	reply.Token = trace.GenerateToken()
+}
+
+// Put is a blocking async RPC from the Frontend
+// saving a new value to storage and memory
+func (s *Storage) Put(args StoragePutArgs, reply *StoragePutReply) {
+	trace := s.tracer.ReceiveToken(args.Token)
+
+	trace.RecordAction(StoragePut{
+		Key: args.Key,
+		Value: args.Value,
+	})
+
+	s.memory.Put(args.Key, args.Value)
+	//TODO: Save to disk
+
+	trace.RecordAction(StorageSaveData{
+		Key: args.Key,
+		Value: args.Value,
+	})
+
+	reply.Token = trace.GenerateToken()
+}
+
